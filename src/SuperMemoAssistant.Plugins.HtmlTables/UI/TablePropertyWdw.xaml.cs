dbbe0076a925973fcf92553c3c94f409e8d3100a
james <james@DESKTOP-JDJA6NK>
2020-07-14 05:30:00 +0100

    Begin adding UI logic

--- src/SuperMemoAssistant.Plugins.HtmlTables/UI/TablePropertyWdw.xaml.cs
+++ src/SuperMemoAssistant.Plugins.HtmlTables/UI/TablePropertyWdw.xaml.cs
@@ -24,9 +24,45 @@ namespace SuperMemoAssistant.Plugins.HtmlTables.UI
     public bool Confirmed { get; set; } = false;
     public HtmlTableProperty Value { get; set; }
 
+    // Values
+    public string TableCaption { get; set; } = string.Empty;
+    public string BorderSize { get; set; } = "2";
+    public string TableRows { get; set; } = "3";
+    public string TableColumns { get; set; } = "3";
+    public string TableWidth { get; set; } = "50";
+    public string CellPadding { get; set; } = "1";
+    public string CellSpacing { get; set; } = "2";
+
     public TablePropertyWdw(HtmlTableProperty props)
     {
+
       InitializeComponent();
+
+
+      DataContext = this;
+
+    }
+
+    private void CancelButtonClick(object sender, RoutedEventArgs e)
+    {
+
+      Confirmed = false;
+      Close();
+
+    }
+
+    private void CreateButtonClick(object sender, RoutedEventArgs e)
+    {
+
+      //Value = new HtmlTableProperty();
+      //Value.CaptionText = TableCaptionBox.Text ?? string.Empty;
+
+      //if (CaptionAlignmentDefault.IsChecked == true)
+      //  Value.CaptionAlignment = HorizontalAlignOption.Default;
+      //else if (CaptionAlignmentDefault)
+      //Value.CaptionAlignment = 
+
+
     }
   }
 }
